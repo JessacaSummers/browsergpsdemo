2e260937ac3bc106ea391ef1ed27a0302ccf9249
JessacaSummers <jessaca.summers@gmail.com>
2023-07-02 17:00:19 -0500

    More map and data

---
+++ src/components/WhitewaterMap.js
@@ -0,0 +1,52 @@
+import React, {useState, useEffect, useCallback, useRef, Fragment} from 'react';
+import { GoogleMap, useJsApiLoader, useLoadScript, MarkerF } from '@react-google-maps/api';
+
+console.log("Api key:", process.env.REACT_APP_GOOGLE_API)
+
+export default function WhitewaterMap ({coords}){
+
+	//const [map, setMap] = React.useState(null);
+
+	const { isLoaded, loadError } = useLoadScript({
+	    id: 'google-map-script',
+	    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API,
+	})
+
+	const mapRef = useRef();
+  	const onMapLoad = useCallback((map) => {
+    	mapRef.current = map;
+  	}, []);
+
+	/*useEffect(() => {
+
+		return () => {
+			setMap(null)
+		}
+	}, [])
+
+	useEffect(() => {
+		const bounds = new window.google.maps.LatLngBounds(coords);
+	    map.fitBounds(bounds);
+	    setMap(map)
+	}, [JSON.stringify(coords)])
+
+	console.log("Map coords:", coords);*/
+
+  return (
+      isLoaded ? <GoogleMap
+      	mapContainerStyle={{ height: '50vh', width: '100%' }}
+        center={coords}
+        zoom={20}
+        center={coords}
+        onLoad={onMapLoad}
+        options={{
+        	//disableDefaultUI: true,
+        	//zoomControl: true
+        }}
+      >
+      	<MarkerF
+            position={coords}
+        />
+      </GoogleMap> : null
+  );
+}
